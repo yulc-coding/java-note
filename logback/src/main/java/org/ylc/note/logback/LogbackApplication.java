e772acbcaabfeb7089f85a67f537022fde5faebe
yulc <350009335@qq.com>
2019-11-10 22:52:04 +0800

    logback

---
+++ logback/src/main/java/org/ylc/note/logback/LogbackApplication.java
@@ -0,0 +1,13 @@
+package org.ylc.note.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackApplication.class, args);
+    }
+
+}
