5468c3e37e0b65bfe5dde08c14690ebd247ce98b
superylcfly <350009335@qq.com>
2019-11-13 17:00:48 +0800

    log4j2

---
+++ log4j2/src/main/java/org/ylc/note/log4j2/service/MyService.java
@@ -0,0 +1,27 @@
+package org.ylc.note.log4j2.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * 代码千万行，注释第一行，
+ * 注释不规范，同事泪两行。
+ *
+ * @author YuLc
+ * @version 1.0.0
+ * @date 2019/11/13
+ */
+@Service
+public class MyService {
+
+    private final Logger logger = LoggerFactory.getLogger(MyService.class);
+
+
+    public void myLog() {
+        logger.debug("this is debug service log");
+        logger.info("this is info service log");
+        logger.error("this is error service log");
+    }
+
+}
--- log4j2/src/test/java/org/ylc/note/log4j2/Log4j2ApplicationTests.java
+++ log4j2/src/test/java/org/ylc/note/log4j2/Log4j2ApplicationTests.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.ylc.note.log4j2.controller.LogTest;
+import org.ylc.note.log4j2.service.MyService;
 
 @SpringBootTest
 class Log4j2ApplicationTests {
@@ -20,16 +21,23 @@ class Log4j2ApplicationTests {
      */
     private final Logger scheduleLogger = LoggerFactory.getLogger("schedule");
 
+    /**
+     * 异步处理日志
+     */
+    private final Logger asyncLogger = LoggerFactory.getLogger("asyncLog");
+
+    @Autowired
+    private MyService myService;
 
     /**
      * 普通日志测试
      */
     @Test
     void normalLogTest() {
+        normalLogger.debug("this is a debug log with out args");
         normalLogger.info("this is a info log with out args");
         normalLogger.error("this is a error log with out args");
     }
-
 
     /**
      * 带参数的日志测试
@@ -38,18 +46,34 @@ class Log4j2ApplicationTests {
     void argsLogTest() {
         MDC.put("user", "鱼大仙");
         MDC.put("args", "参数");
+        normalLogger.debug("this is a debug log with args");
         normalLogger.info("this is a info log with args");
         normalLogger.error("this is a error log with args");
     }
 
-
     /**
-     * 监控类日志测试
+     * 独立日志测试
+     * 1、指定日志名称
+     * 2、指定包或者类
      */
     @Test
-    void scheduleLogTest() {
+    void independentLogTest() {
+        // 指定名称
+        scheduleLogger.debug("this is a schedule debug log");
         scheduleLogger.info("this is a schedule info log");
-        scheduleLogger.info("this is a schedule error log");
+        scheduleLogger.error("this is a schedule error log");
+        // 指定包或者类
+        myService.myLog();
+    }
+
+    /**
+     * 异步日志
+     */
+    @Test
+    void asyncLogTest() {
+        asyncLogger.debug("this is a async log -- debug");
+        asyncLogger.info("this is a begin async log -- info");
+        asyncLogger.error("this is a begin async log -- error");
     }
 
 }
