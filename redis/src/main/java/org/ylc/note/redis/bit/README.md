c0532252cdd557b741da801e7a5a84084ab012ae
superylcfly <350009335@qq.com>
2020-01-17 14:18:35 +0800

    init

--- redis/src/main/java/org/ylc/note/redis/bit/README.md
+++ redis/src/main/java/org/ylc/note/redis/bit/README.md
@@ -1,4 +1,112 @@
+![](https://github.com/yulc-coding/java-note/blob/master/QR_code.png)
+
 ## 位图
+最大可以设置2^32个不同的位，即512MB
+
+位图的存储大小计算： （maxOffset / 8 / 1024 / 1024）MB。其中maxOffset为位图的最大位数
+
+### 基本用法
+#### SETBIT key offset value
+设置指定key的值在offset处的bit值，offset从0开始。返回值为在offset处原来的bit值
+```
+# 通过位操作将 h 改成 i
+127.0.0.1:6379> SET h h         # 二进制为 01101000
+OK
+127.0.0.1:6379> SETBIT h 7 1    # 将最后一位改成1 => 01101001
+(integer) 0
+127.0.0.1:6379> GET h
+"i"
+```
+
+#### GETBIT key offset
+获取指定key的值在offset处的bit值，offset从0开始。如果offset超出看当前位图的范围，则返回0。
+```
+127.0.0.1:6379> set i i       # 二进制为 01101001
+OK
+127.0.0.1:6379> getbit i 0    # 第1位为0
+(integer) 0
+127.0.0.1:6379> getbit i 1    # 第2位为0
+(integer) 1
+127.0.0.1:6379> getbit i 7    # 第8位为0
+(integer) 1
+```
+
+#### BITCOUNT key [start end]
+统计指定key值中被设置为1的bit数。可以通过指定参数star和end来限制统计范围。
+> 注意，这里的star和end不是指bit的下标，而是字节(byte)的下标。比如start为1，则实际对应的bit下标为8（1byte = 8 bit）
+
+```
+127.0.0.1:6379> set hi hi           # 二进制为 0110100001101001
+OK
+127.0.0.1:6379> bitcount hi         # 所有是1的位数：7个
+(integer) 7
+127.0.0.1:6379> bitcount hi 1 2     # 即统计 01101001 中1的位数
+(integer) 4
+```
+
+#### BITPOS key bit [start] [end]
+统计首次出现的0或1的bit位，可以通过start和end来指定范围，同样是指字节的下标。
+* 在不存在的key或者空字符串中查找1，则返回-1
+* 在所有bit都为1中查找bit为0的情况下，返回字符串最右边的第一个空位
+
+```
+    127.0.0.1:6379> get nilkey           # 不存在的key
+    (nil)
+    127.0.0.1:6379> bitpos nilkey 1      # 在不存在的key中查首次出现1的位
+    (integer) -1
+    127.0.0.1:6379> setbit nilkey 0 0    # 空字符串
+    (integer) 0
+    127.0.0.1:6379> get nilkey
+    "\x00"
+    127.0.0.1:6379> bitpos nilkey 1
+    (integer) -1
+```
+
+#### BITOP operation destkey key [key ...]
+对一个或多个二进制位字符串进行操作，并将结果保存到 destkey 上。当某个字符串长度不够时，对应的位用0补上
+* AND（逻辑与）：都为1返回1，否则返回0
+```
+    127.0.0.1:6379> set a a                  # 二进制  01100001
+    OK
+    127.0.0.1:6379> set c c                  # 二进制  01100011
+    OK
+    127.0.0.1:6379> bitop and destkey a c    # 与操作  01100001 -> a
+    (integer) 1
+    127.0.0.1:6379> get destkey
+    "a"
+```
+
+* OR（逻辑或）：只要有一个1就返回1，否则返回0
+```
+    127.0.0.1:6379> set a a                 # 二进制  01100001
+    OK
+    127.0.0.1:6379> set b b                 # 二进制  01100010
+    OK
+    127.0.0.1:6379> bitop or destkey a b    # 或操作  01100011 -> c
+    (integer) 1
+    127.0.0.1:6379> get destkey
+    "c"
+    127.0.0.1:6379>     
+```
+
+* XOR（逻辑异或）：当都是0或者都是1时返回0，否则返回1
+```
+    127.0.0.1:6379> set a a                 # 二进制  01100001
+    OK
+    127.0.0.1:6379> set z Z                 # 二进制  01011010 (大写的Z)
+    OK
+    127.0.0.1:6379> bitop xor destkey a z   # 异或    00111011 -> ; 分号
+    (integer) 1
+    127.0.0.1:6379> get destkey
+    ";"
+```
+
+* NOT（逻辑非）：取反，1变成0，0变成1。**只能传入一个要操作的key**
+```
+    01010101 -> 10101010
+```
+
+
 
 ### 统计用户上线次数
 
--- redis/src/main/java/org/ylc/note/redis/bit/controller/BitMapController.java
+++ redis/src/main/java/org/ylc/note/redis/bit/controller/BitMapController.java
@@ -5,14 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.ylc.note.redis.RedisService;
+import org.ylc.note.redis.bit.service.RedisService;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.BitSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 代码千万行，注释第一行，
@@ -28,16 +27,27 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RequestMapping("/redis/bit")
 public class BitMapController {
 
+    private final DateTimeFormatter formatters = DateTimeFormatter.ofPattern("yyyyMMdd");
+
     /**
      * 定义签到前缀
      * key格式为 sing:{yyyyMMdd}
      */
     private static final String SIGN_PREFIX = "sign:";
 
+    /**
+     * 连续一周签到
+     */
     private static final String SIGN_ALL_WEEK_KEY = "signAllWeek";
 
+    /**
+     * 连续一个月签到
+     */
     private static final String SIGN_ALL_MONTH_KEY = "signAllMonth";
 
+    /**
+     * 一周内有签到过的
+     */
     private static final String SIGN_IN_WEEK_KEY = "signInWeek";
 
     private final RedisService redisService;
@@ -47,7 +57,40 @@ public class BitMapController {
     }
 
     /**
-     * 按日期记录签到信息
+     * 初始化本年今天之前的测试数据
+     */
+    @GetMapping("/init")
+    public void initData() {
+        // 获取本年的日期列表
+        List<String> dateKeyList = new ArrayList<>();
+        LocalDate curDate = LocalDate.now();
+        LocalDate beginDate = LocalDate.parse("2020-01-01");
+        while (beginDate.isBefore(curDate)) {
+            dateKeyList.add(SIGN_PREFIX + beginDate.format(formatters));
+            beginDate = beginDate.plusDays(1);
+        }
+        // 是否签到
+        boolean isSign;
+        StringBuilder signInfo;
+        for (int i = 1; i < 6; i++) {
+            signInfo = new StringBuilder("用户【").append(i).append("】：");
+            for (String dateKey : dateKeyList) {
+                if (i == 1) {
+                    // 用户1全部签到
+                    isSign = true;
+                } else {
+                    // 其他用户随机
+                    isSign = Math.random() > 0.5;
+                }
+                redisService.setBit(dateKey, i, isSign);
+                signInfo.append(isSign ? 1 : 0).append(", ");
+            }
+            System.out.println(signInfo.toString());
+        }
+    }
+
+    /**
+     * 用户当天签到
      * 用户ID作为位图的偏移量
      */
     @GetMapping("/sign/{userId}")
@@ -70,7 +113,6 @@ public class BitMapController {
 
     /**
      * 统计今天所有的签到数量
-     * 即统计
      */
     @GetMapping("/todayCount")
     public String todayCount() {
@@ -78,24 +120,31 @@ public class BitMapController {
     }
 
     /**
-     * 获取用户全年的签到数
+     * 统计指定用户全年的签到数
      */
     @GetMapping("/userYearSign/{userId}")
     public String userYearSign(@PathVariable Long userId) {
         int year = LocalDate.now().getYear();
         // 获取所有的key
         Set<String> keys = redisService.getKeys(SIGN_PREFIX + year + "*");
-        // 使用BitSet 去存储用户每天的签到信息
-        BitSet users = new BitSet();
-        AtomicInteger index = new AtomicInteger();
-        keys.forEach(key -> users.set(index.getAndIncrement(), redisService.getBit(key, userId)));
-
-        int signCount = users.cardinality();
+        /*
+         * 可以使用BitSet 去存储用户每天的签到信息，用于其他的操作
+         * BitSet users = new BitSet();
+         * 统计所有已经签到的数量 对应 redis的bitCount
+         * users.cardinality()
+         */
+        int signCount = 0;
+        for (String key : keys) {
+            if (redisService.getBit(key, userId)) {
+                signCount++;
+            }
+        }
         return String.format("本年已累计签到： %d 次", signCount);
     }
 
     /**
-     * 近7天连续签到的用户数量
+     * 统计近7天连续签到的用户数量
+     * 逻辑与
      */
     @GetMapping("/signAllWeek")
     public String signAllWeek() {
@@ -105,20 +154,21 @@ public class BitMapController {
     }
 
     /**
-     * 本月全部签到过的用户数量
+     * 统计本月全部签到过的用户数量
      */
     @GetMapping("/signAllMonth")
     public String signAllMonth() {
         redisService.bitOp(
                 RedisStringCommands.BitOperation.AND,
                 SIGN_ALL_MONTH_KEY,
-                SIGN_PREFIX + LocalDate.now().getYear()
+                SIGN_PREFIX + LocalDate.now().getYear() + "*"
         );
         return String.format("月全部签到过的用户数：%d", redisService.bitCount(SIGN_ALL_MONTH_KEY));
     }
 
     /**
-     * 近7天有过签到的用户数量，只签到1次也算
+     * 统计近7天有过签到的用户数量，只签到1次也算
+     * 逻辑或
      */
     @GetMapping("/signInWeek")
     public String signInWeek() {
@@ -133,7 +183,7 @@ public class BitMapController {
      * @return yyyyMMdd
      */
     private String getCurDate() {
-        return LocalDate.now().toString().replace("-", "");
+        return LocalDate.now().format(formatters);
     }
 
     /**
@@ -142,9 +192,9 @@ public class BitMapController {
     private List<String> getWeekKeys() {
         List<String> dateList = new ArrayList<>();
         LocalDate curDate = LocalDate.now();
-        dateList.add(SIGN_PREFIX + curDate.toString().replace("-", ""));
+        dateList.add(SIGN_PREFIX + curDate.format(formatters));
         for (int i = 1; i < 7; i++) {
-            dateList.add(SIGN_PREFIX + curDate.plusDays(-i).toString().replace("-", ""));
+            dateList.add(SIGN_PREFIX + curDate.plusDays(-i).format(formatters));
         }
         return dateList;
     }
