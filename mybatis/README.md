38df1177d93cde3f9519abe2981e1c2206cb043d
superylcfly <350009335@qq.com>
2020-06-08 08:39:01 +0800

    Mybatis

---
+++ mybatis/README.md
@@ -0,0 +1,1 @@
+### Spring Boot整合Mybatis
---
+++ mybatis/src/test/java/org/ylc/note/mybatis/MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.note.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
