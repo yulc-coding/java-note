9a91e7c60760336a627af3b145d828976a68a2ae
yulichao <yulichao@travelsky.com>
2020-07-20 22:49:54 +0800

    ShardingSphere 分表分库

---
+++ sharding-sphere/src/main/java/org/ylc/note/shardingsphere/ShardingSphereApplication.java
@@ -0,0 +1,13 @@
+package org.ylc.note.shardingsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingSphereApplication.class, args);
+    }
+
+}
---
+++ sharding-sphere/src/test/java/org/ylc/note/shardingsphere/ShardingSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.note.shardingsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
