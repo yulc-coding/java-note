110431a2e964b87fe50e54ca2891a2762e54c620
yulc <350009335@qq.com>
2020-08-25 23:00:18 +0800

    quartz

---
+++ quartz/README.md
@@ -0,0 +1,11 @@
+## 任务调度
+[官网](http://www.quartz-scheduler.org/documentation/quartz-2.3.0/quick-start.html)
+
+### 任务`JOB`
+定义要具体执行的内容
+
+### 触发器`Trigger`
+用于触发执行`JOB`
+
+### 调度器`Scheduler`
+独立运行容器，控制`JOB`和 `Trigger`
---
+++ quartz/src/test/java/org/ylc/note/quartz/QuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.note.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
