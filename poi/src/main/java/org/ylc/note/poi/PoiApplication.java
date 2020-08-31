3faf7ce7ea7ccb1b1f3b947125c09233dcc33292
yulc <350009335@qq.com>
2020-08-31 22:22:42 +0800

    poi

---
+++ poi/src/main/java/org/ylc/note/poi/PoiApplication.java
@@ -0,0 +1,13 @@
+package org.ylc.note.poi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoiApplication.class, args);
+    }
+
+}
---
+++ poi/src/test/java/org/ylc/note/poi/PoiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.note.poi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
