16544d694eeab645941848fb00724a4dbfa6fb18
Yulc <350009335@qq.com>
2021-01-11 21:55:31 +0800

    Netty-Websocket ： 消息处理

--- netty/netty-websocket/src/main/java/org/ylc/note/netty/websocket/handler/SocketMessageHandler.java
+++ netty/netty-websocket/src/main/java/org/ylc/note/netty/websocket/handler/SocketMessageHandler.java
@@ -14,8 +14,46 @@ import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
  * @date 2021/1/11
  */
 public class SocketMessageHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+
+    /**
+     * 建立连接是触发
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+    }
+
+    /**
+     * 断开连接是触发
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        super.channelInactive(ctx);
+    }
+
+    /**
+     * 服务端接受客户端消息
+     *
+     * @param channelHandlerContext
+     * @param textWebSocketFrame
+     * @throws Exception
+     */
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
+
+    }
+
+    /**
+     * 给客户端发送消息
+     */
+    private void sendMessage() {
 
     }
 }
