8680f235e7dcc0a893ed55eb6d7dd6ae714b586d
Yulc <350009335@qq.com>
2021-01-11 21:03:17 +0800

    Netty-Websocket

---
+++ netty/netty-websocket/src/main/java/org/ylc/note/netty/websocket/NettyWebsocketApplication.java
@@ -0,0 +1,13 @@
+package org.ylc.note.netty.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyWebsocketApplication.class, args);
+	}
+
+}
---
+++ netty/netty-websocket/src/test/java/org/ylc/note/netty/websocket/NettyWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.note.netty.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
