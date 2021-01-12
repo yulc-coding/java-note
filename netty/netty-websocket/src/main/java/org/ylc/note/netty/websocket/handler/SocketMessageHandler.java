f5a262bd043f03e1751ccbb811d9debece32c52e
Yulc <350009335@qq.com>
2021-01-12 21:48:03 +0800

    Netty-Websocket

--- netty/netty-websocket/src/main/java/org/ylc/note/netty/websocket/handler/SocketMessageHandler.java
+++ netty/netty-websocket/src/main/java/org/ylc/note/netty/websocket/handler/SocketMessageHandler.java
@@ -1,8 +1,15 @@
 package org.ylc.note.netty.websocket.handler;
 
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.util.concurrent.GlobalEventExecutor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 /**
  * 代码千万行，注释第一行，
@@ -13,47 +20,54 @@ import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
  * @author Yulc
  * @date 2021/1/11
  */
+@Slf4j
+@Component
+@ChannelHandler.Sharable
 public class SocketMessageHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
+    public static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
     /**
-     * 建立连接是触发
-     *
-     * @param ctx
-     * @throws Exception
+     * 建立连接时触发
      */
     @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        super.channelActive(ctx);
+    public void channelActive(ChannelHandlerContext ctx) {
+        log.info("新增客户端：{}", ctx.channel().localAddress());
+        channelGroup.add(ctx.channel());
+        sendMessage(ctx.channel(), "connect success!");
     }
 
     /**
-     * 断开连接是触发
-     *
-     * @param ctx
-     * @throws Exception
+     * 断开连接时触发
      */
     @Override
-    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        super.channelInactive(ctx);
+    public void channelInactive(ChannelHandlerContext ctx) {
+        log.info("客户端[{}]断开连接", ctx.channel().localAddress());
+        channelGroup.remove(ctx.channel());
     }
 
     /**
      * 服务端接受客户端消息
-     *
-     * @param channelHandlerContext
-     * @param textWebSocketFrame
-     * @throws Exception
      */
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
-
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame textWebSocketFrame) {
+        String clientMsg = textWebSocketFrame.text();
+        log.info("客户端【{}】发送信息：{}", ctx.channel().localAddress(), clientMsg);
     }
 
     /**
      * 给客户端发送消息
      */
-    private void sendMessage() {
+    private void sendMessage(Channel channel, String message) {
+        channel.writeAndFlush(new TextWebSocketFrame(message));
+    }
 
+    /**
+     * 批量发送
+     *
+     * @param message 消息
+     */
+    public void sendMessageBatch(String message) {
+        channelGroup.writeAndFlush(new TextWebSocketFrame(message));
     }
 }
--- netty/netty-websocket/src/main/java/org/ylc/note/netty/websocket/server/NettyServer.java
+++ netty/netty-websocket/src/main/java/org/ylc/note/netty/websocket/server/NettyServer.java
@@ -11,59 +11,77 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.ylc.note.netty.websocket.handler.SocketMessageHandler;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * 代码千万行，注释第一行，
  * 注释不规范，同事泪两行。
  * <p>
  * 服务端
+ * <p>
+ * webSocket测试地址：
+ * http://www.easyswoole.com/wstool.html
  *
  * @author Yulc
  * @date 2021/1/11
  */
 @Slf4j
+@Component
 public class NettyServer {
 
     @Value("9000")
     private int port;
 
+    @Autowired
+    private SocketMessageHandler socketMessageHandler;
+
+    EventLoopGroup bossGroup = new NioEventLoopGroup();
+    EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+    @PostConstruct
     public void start() throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
 
-        try {
-            ServerBootstrap bootstrap = new ServerBootstrap();
-            bootstrap.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .localAddress(port)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel socketChannel) throws Exception {
-                            log.info("收到新的连接信息：【{}】", socketChannel.localAddress());
-                            socketChannel.pipeline()
-                                    // Http 解码器
-                                    .addLast("codec", new HttpServerCodec())
-                                    // HTTP 消息聚合
-                                    .addLast("aggregator", new HttpObjectAggregator(65536))
-                                    // webSocket 相关
-                                    .addLast(new WebSocketServerProtocolHandler("/socketServer", "WebSocket", true, 65536 * 10))
-                                    // 自定义业务逻辑处理器
-                                    .addLast("socketHandler", new SocketMessageHandler());
-                        }
-                    })
-                    .option(ChannelOption.SO_BACKLOG, 1024);
-            // 端口绑定
-            ChannelFuture future = bootstrap.bind().sync();
-            log.info("webSocket server started, Listening on : {}", port);
-            // 关闭服务器通道 会 wai
-            future.channel().closeFuture().sync();
-        } finally {
-            workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
-        }
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .localAddress(port)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        log.info("收到新的连接信息：【{}】", socketChannel.localAddress());
+                        socketChannel.pipeline()
+                                // Http 解码器
+                                .addLast("codec", new HttpServerCodec())
+                                // 分块处理
+                                .addLast("chunked", new ChunkedWriteHandler())
+                                // HTTP 消息聚合
+                                .addLast("aggregator", new HttpObjectAggregator(65536))
+                                // webSocket 相关
+                                .addLast(new WebSocketServerProtocolHandler("/socketServer", "WebSocket", true, 65536 * 10))
+                                // 自定义业务逻辑处理器
+                                .addLast("socketHandler", socketMessageHandler);
+                    }
+                })
+                .option(ChannelOption.SO_BACKLOG, 1024);
+        // 端口绑定
+        ChannelFuture future = bootstrap.bind().sync();
+        log.info("webSocket server started, Listening on : {}", port);
+        // 关闭服务器通道 会 wait
+        // future.channel().closeFuture().sync();
+    }
 
+
+    @PreDestroy
+    public void destroy() {
+        workerGroup.shutdownGracefully();
+        bossGroup.shutdownGracefully();
     }
 }
